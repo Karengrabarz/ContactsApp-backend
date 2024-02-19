f4a4979f454366a93ddc4fa2e7792dc2e03a91a7
Karengrabarz <129179855+Karengrabarz@users.noreply.github.com>
2024-02-19 09:33:34 -0300

    Delete dist directory

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateId = void 0;
-let id = 1;
-const generateId = () => {
-    return id++;
-};
-exports.generateId = generateId;

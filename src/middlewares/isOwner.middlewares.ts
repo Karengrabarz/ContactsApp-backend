2404143283183f24252243e9d2acdfab8699b90d
Karengrabarz <karengrabarz@gmail.com>
2024-02-19 09:34:03 -0300

    fix: gitignore

---
+++ src/middlewares/isOwner.middlewares.ts
@@ -0,0 +1,5 @@
+import { NextFunction, Request, Response } from "express"
+import { AppDataSource } from "../data-source"
+import { Contact } from "../entities/contacts.entity"
+import { AppError } from "../errors/AppError"
+
